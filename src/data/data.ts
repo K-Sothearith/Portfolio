3f5eeb3471e4d676986de6ae9fadc70fbf4e6956
K-Rithz <sothearith.kong@student.cadt.edu.kh>
2026-05-17 18:21:39 +0700

    Adding data

---
+++ src/data/data.ts
@@ -0,0 +1,107 @@
+import type { TechStack } from './type';
+
+export const TechStackData: TechStack[] = [
+  {
+    name: "HTML",
+    // icon: html,
+    tag: "Language",
+    // variant: "primary"
+  },
+  {
+    name: "CSS",
+    // icon: css,
+    tag: "Language",
+    // variant: "primary"
+  },
+  {
+    name: "JavaScript",
+    // icon: javascript,
+    tag: "Language",
+    // variant: "primary"
+  },
+  {
+    name: "TypeScript",
+    // icon: typescript,
+    tag: "Language",
+    // variant: "primary"
+  },
+  {
+    name: "React JS",
+    // icon: reactjs,
+    tag: "Framework",
+    // variant: "primary"
+  },
+  {
+    name: "Tailwind",
+    // icon: tailwind,
+    tag: "Framework",
+    // variant: "primary"
+  },
+  {
+    name: "Node JS",
+    // icon: nodejs,
+    tag: "Runtime",
+    // variant: "primary"
+  },
+  {
+    name: "Figma",
+    // icon: figma,
+    tag: "Tool",
+    // variant: "primary"
+  },
+  {
+    name: "Git",
+    // icon: git,
+    tag: "Tool",
+    // variant: "primary"
+
+  },
+  {
+    name: "GitHub",
+    // icon: github,
+    tag: "Platform",
+    // variant: "primary"
+  },
+  {
+    name: "MySQL",
+    // icon: mysql,
+    tag: "Database",
+    // variant: "primary"
+  },
+  {
+    name: "Java",
+    // icon: java,
+    tag: "Language",
+    // variant: "secondary"
+  },
+  {
+    name: "C",
+    // icon: c,
+    tag: "Language",
+    // variant: "secondary"
+  },
+  {
+    name: "C++",
+    // icon: cplus,
+    tag: "Language",
+    // variant: "secondary"
+  },
+  {
+    name: "Vite",
+    // icon: vite,
+    tag: "Tool",
+    // variant: "primary"
+  },
+  {
+    name: "Vercel",
+    // icon: vercel,
+    tag: "Platform",
+    // variant: "primary"
+  },
+  {
+    name: "Three.js",
+    // icon: threejs,
+    tag: "Library",
+    // variant: "secondary"
+  }
+];
